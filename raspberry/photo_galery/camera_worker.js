e43ac4047060401c2c57ba430eb94632f395cf51
João Gonçalves <joaojagoncalves10@gmail.com>
2022-05-25 14:34:19 +0100

    Photo Galery Update

--- raspberry/photo_galery/camera_worker.js
+++ raspberry/photo_galery/camera_worker.js
@@ -1,50 +1,87 @@
 const { parentPort, workerData } = require("worker_threads");
 
-// Computer camera setting bellow
+// Firebase
+{
+    const { initializeApp } = require("firebase/app");
+    const { getStorage, ref, uploadBytes } = require("firebase/storage");
+    const fs = require("fs");
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyApxRfbs6ESwc-g_FVIk7AjznWV0AxG8WY",
+        authDomain: "doorbell-76f1f.firebaseapp.com",
+        projectId: "doorbell-76f1f",
+        storageBucket: "doorbell-76f1f.appspot.com",
+        messagingSenderId: "718748135646",
+        appId: "1:718748135646:web:b2487753e716f76a6f524f"
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    var storage = getStorage(app);
+
+    function storeFireBase() {
+        let time = Date.now().toString();
+        // Create a reference to 'mountains.jpg'
+        const imageRef = ref(storage, time + ".jpeg");
+
+        // Create a reference to 'images/mountains.jpg'
+        const fullImageRef = ref(storage, 'photos/' + time + ".jpeg");
+
+        // 'file' comes from the Blob or File API
+        uploadBytes(imageRef, fs.readFileSync('./photos/1.jpeg'))
+            .then((snapshot) => {
+                console.log(snapshot);
+            });
+    }
+}
+
+
+// Computer camera settings bellow
 {
     const NodeWebcam = require("node-webcam");
     var opts = {
-    width: 1280,
-    height: 720,
-    quality: 100,
-    frames: 1,
-    saveShots: true,
-    output: "jpeg",
-    
-    //Which camera to use
-    //Use Webcam.list() for results
-    //false for default device
-    device: false,
+        width: 1280,
+        height: 720,
+        quality: 100,
+        frames: 1,
+        saveShots: true,
+        output: "jpeg",
 
-    // [location, buffer, base64]
-    // Webcam.CallbackReturnTypes
-    callbackReturn: "location",    
-    
-    //Logging
-    verbose: false
-};
-var Webcam = NodeWebcam.create( opts );
+        //Which camera to use
+        //Use Webcam.list() for results
+        //false for default device
+        device: false,
+
+        // [location, buffer, base64]
+        // Webcam.CallbackReturnTypes
+        callbackReturn: "location",
+
+        //Logging
+        verbose: false
+    };
+    var Webcam = NodeWebcam.create(opts);
 }
 
-var pictureId = 0;
-function takePicture(){
-    Webcam.capture("./photos/" + pictureId + ".jpeg", function( err, data ) {} );
-    pictureId += 1;
+function takePicture() {
+    let time = Date.now().toString()
+    Webcam.capture("./photos/" + time + ".jpeg", function (err, data) { });
+    //storeFireBase();
     console.log("Picture Taken by Worker!")
 }
 
 var interval;
-parentPort.on("message", function(msg){
+parentPort.on("message", function (msg) {
     parentPort.postMessage(msg)
     let message = msg.data;
     let timeOutInterval = msg.timeOutInterval * 1000;
 
-    if(message == "takePicture")
+    if (message == "startCapture"){
+        clearInterval(interval);
         interval = setInterval(function () {
-            parentPort.postMessage('Picture Taken!');
             takePicture();
         }, timeOutInterval);
-    else{
+    }
+    else {
         clearInterval(interval);
     }
 })
--- raspberry/photo_galery/index.js
+++ raspberry/photo_galery/index.js
@@ -12,20 +12,67 @@ app.use("/photos", express.static(photo_dir));
  * Send an array of all photos as a
  * response to get requests to "/photos"
  */
+const ip = "localhost";
+const url = ip + ":3000";
 app.get("/photos", (req, res) => {
-    res.send(fs.readdirSync(photo_dir));
+    let obj = []
+    let new_obj = {}
+    for (photo_name of fs.readdirSync(photo_dir)) {
+        new_obj = {
+            img_src: "http://" + url + "/photos/" + photo_name,
+        }
+        obj.push(new_obj)
+    }
+    res.send(obj);
+});
+
+app.delete("/photos/:fileName", (req, res) => {
+    fs.unlink("./photos/" + req.params.fileName, function (err) {
+        if (err) throw err;
+        // if no error, file has been deleted successfully
+        console.log('File deleted!');
+    });
+    console.log("File DELETED: " + req.params.fileName);
+    res.send("File Deleted");
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
 });
 
+var interval;
 const workerObj = require("./camera_main").newCameraWorker();
 const timeOutInterval = 5;
-workerObj.worker.postMessage({ data: "takePicture", timeOutInterval: timeOutInterval })
+const timeOutIntervalMinutes = 5;
+
+app.post("/camera/startCapture", (req, res) => {
+    if (workerObj != null) {
+        console.log("Starting Capture...")
+        workerObj.worker.postMessage({ data: "startCapture", timeOutInterval: timeOutInterval })
+    }
+    res.send("Capture Started");
+});
+
+app.post("/camera/resetCaptureTimer", (req, res) => {
+    console.log("Resetting " + timeOutIntervalMinutes + "minute Capture Timer...")
+    interval = setInterval(function () {
+        if (workerObj != null)
+            workerObj.worker.postMessage({ data: "stopCapture" });
+        clearInterval(interval);
+    }, timeOutIntervalMinutes * 60 * 1000);
+    res.send("Timer Reset");
+});
+
+app.post("/camera/stopCapture", (req, res) => {
+    if (workerObj != null) {
+        console.log("Stopping Capture...")
+        workerObj.worker.postMessage({ data: ".." });
+    }
+    res.send("Capture Stopped");
+});
 
 /* In case you want to test if the singleton works, run this function */
-function testSingleton_CameraWorker(){
+function testSingleton_CameraWorker() {
     console.log(workerObj);
     const workerObj2 = require("./camera_main").newCameraWorker();
     console.log(workerObj2);
